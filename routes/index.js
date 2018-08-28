96b6937831a51e07cfabcd412e3c8a15c1a11839
Deitro Dazo <thesilentelegy@users.noreply.github.com>
2018-08-29 06:54:17 +0800

    Remove unnecessary route handlers

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).send({
-    message: 'Welcome to SAGE',
-  });
-});
-
-router.get('/api/v1', (req, res) => {
-  res.status(200).send({
-    message: 'Welcome to SAGE',
-  });
+  res.send('SAGE');
 });
 
 module.exports = router;
